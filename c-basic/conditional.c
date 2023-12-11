701fc20cc5ee9371edc82ec847b09434bf332c0b
djscrappykoko <153549860+djscrappykoko@users.noreply.github.com>
2023-12-11 20:24:21 +0100

    conditional.c

---
+++ c-basic/conditional.c
@@ -0,0 +1,20 @@
+#include 
+
+int main() {
+    /* Given values */
+    int a = 5;
+    int b = 7;
+    int c = 9;
+
+    /* Perform arithmetic operations */
+    int sum = a + b + c;
+
+    /* Implement conditional statement */
+    if (sum > c) {
+        printf("Sum is greater than c\n");
+    } else {
+        printf("Sum is not greater than c\n");
+    }
+
+    return (0);
+}
